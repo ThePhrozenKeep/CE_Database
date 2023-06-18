748c867d86d71625c94a0697a35162aaacc8a77f
Lectem <lectem@gmail.com>
2023-06-19 01:04:51 +0200

    Update import .def file script (used to sync from d2moo)

--- scripts/IDA/import-def-file.py
+++ scripts/IDA/import-def-file.py
@@ -1,60 +1,114 @@
 # -----------------------------------------------------------------------
-# This is an example illustrating how to enumerate imports
-# (c) Hex-Rays
-#
+# Small utility to import names from .def files using ordinals.
+# -----------------------------------------------------------------------
+
 import idaapi
 import idautils
+import idc
+import ida_kernwin
+import os
 
-def ReadDefFileLine(line):
-    line = line.strip()
-    if not line or line[0] == ';':
-        return
-    name,ordinal = line.split()
-    ordinal = ordinal.replace("@","")
-    if not name or not ordinal:
-        return
-    return (name,int(ordinal))
+def filenameNoExt(filename):
+    return os.path.splitext(os.path.basename(filename))[0]
 
-def ImportDefFileNames(filename):
+def removeprefix_case_insensitive(instr, prefix):
+    if instr.casefold().startswith(prefix.casefold()):
+        return instr[len(prefix):]
+    return instr
+
+def ImportOrdinalNamesFromDef(filename):
     with open(filename,"r") as file:
-        for i, line in enumerate(file):
-            defLine = ReadDefFileLine(line)
-            if defLine:
-                (name,ordinal) = defLine
-                print("Ordinal :{} Name:{}".format(ordinal,name))
-                entry_ea = GetEntryPoint(ordinal)
-                if entry_ea != BADADDR:
-                    MakeName(entry_ea, name)
+        libraryNameFromDef=""
+        while line := file.readline():
+            line = line.strip()
+            if line == "" or line.startswith(';'):
+                continue
+            lineWords = line.split()
+            firstwordUpper = str.upper(lineWords[0])
+            if firstwordUpper =="LIBRARY":
+                libraryNameFromDef = lineWords[1]
+                continue
+            if firstwordUpper =="EXPORTS":
+                break
+            print("Unknown .def format")
+            return
+        if libraryNameFromDef:
+            print(".def file gave LIBRARY=%s" % libraryNameFromDef)
 
-def ReadPrototypeLine(line):
-    line = line.strip()
-    line = line.split('/')[0] #remove comments
-    if not line:
-        return
-    (ordinal,prototype) = line.split(" ",1)
-    return (int(ordinal),prototype)
+        nameFromFile = filenameNoExt(filename)
 
-def ImportEntriesPrototypes(filename):
-    notFoundOrdinals = []
-    with open(filename,"r") as file:
-        for i, line in enumerate(file):
-            defLine = ReadPrototypeLine(line)
-            if defLine:
-                (ordinal,prototype) = defLine
-                entry_ea = GetEntryPoint(ordinal)
-                outputStr = "Ordinal :{} prototype:{}".format(ordinal,prototype)
-                if entry_ea != BADADDR:
-                    if not GetTinfo(entry_ea):
-                        SetType(entry_ea, prototype)
-                else:
-                    notFoundOrdinals.append(outputStr)
-    if len(notFoundOrdinals):
-        print("Unknown ordinals(not in this .dll):")
-        for str in notFoundOrdinals:
-            print(str)
+        libraryIsCurrentFile = False
+        rootFilenameNoExt = filenameNoExt(idc.get_root_filename())
+        if filename.casefold() == rootFilenameNoExt.casefold():
+            libraryIsCurrentFile = True
+        if libraryNameFromDef.casefold() == rootFilenameNoExt.casefold():
+            libraryIsCurrentFile = True
 
-filename = AskFile(forsave=0, mask = "*.def;*.txt", prompt="Choose a .def file to import")
+        nimps = idaapi.get_import_module_qty()
+
+        ordinalToEA = {}
+
+        if libraryIsCurrentFile:
+            print("Importing exports of current file")
+            for index, ordinal, ea, name in idautils.Entries():
+                ordinalToEA[ordinal] = ea
+            moduleName = filenameNoExt(idc.get_root_filename())
+        else:
+            print("Importing imports of current file")
+            def enum_import_callback(ea, name, ordinal):
+                ordinalToEA[ordinal] = ea
+                return True
+            
+            moduleName = ""
+            for moduleIdx in range(0, nimps):
+                name = idaapi.get_import_module_name(moduleIdx)
+                if not name:
+                    print("Failed to get import module name for module #%d" % moduleIdx)
+                    continue
+                if filenameNoExt(name).casefold() == nameFromFile.casefold() or filenameNoExt(name).casefold() == libraryNameFromDef.casefold():
+                    print("Found imports for %s." % name)
+                    moduleName = name
+                    idaapi.enum_import_names(moduleIdx, enum_import_callback)
+                    break
+            if not moduleName:
+                print("Didn't find %s in the imports." % nameFromFile)
+                return
+
+
+        while line := file.readline():
+            line = line.strip()
+            if line == "" or line.startswith(';'):
+                continue
+            lineWords = line.split()
+            if(lineWords[1].startswith('@')):
+                ordinalName = lineWords[0]
+                ordinal = int(lineWords[1][1:])
+                if ordinal in ordinalToEA:
+                    print("%d => %x : %s" %(ordinal, ordinalToEA[ordinal], ordinalName))
+                    
+                    fullprefix = "%s_%d" % (moduleName, ordinal)
+                    if ordinalName.casefold() == fullprefix.casefold():
+                        ordinalName = ""
+                    else:
+                        ordinalName = removeprefix_case_insensitive(ordinalName, fullprefix + "_")
+                        ordinalName = removeprefix_case_insensitive(ordinalName, moduleName + "_")
+                        ordinalName = fullprefix + "_" + ordinalName
+                    if not libraryIsCurrentFile:
+                        if ordinalName: # Don't delete name
+                            idc.set_name(ordinalToEA[ordinal], ordinalName, 0)
+                    else:
+                        if ordinalName: # Don't delete name
+                            idc.rename_entry(ordinal, ordinalName)
+                        #else:
+                        #    idc.rename_entry(ordinal, fullprefix)
+                        #    # Remove previously set name
+                        #    idc.set_name(ordinalToEA[ordinal], "", 0)
+                            
+
+
+
+filename = ida_kernwin.ask_file(False, "*.def;*.txt", "Choose a .def file to import")
 if filename:
-    ImportEntriesPrototypes(filename)
+    ImportOrdinalNamesFromDef(filename)
 else:
     print(".def import was canceled")
